1c505c6075d5c26a5c5ff6f693d763c12aa9c524
robertomsouza08-ctrl <robertomsouza08@gmail.com>
2025-10-29 16:53:57 -0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,24 @@
-
 import NextAuth from 'next-auth'
-import { authOptions } from '../../../lib/auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from '../../../../lib/prisma'
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || '',
+      clientSecret: process.env.GOOGLE_SECRET || '',
+    }),
+  ],
+  pages: {
+    signIn: '/signin',
+  },
+})
+
 export { handler as GET, handler as POST }
