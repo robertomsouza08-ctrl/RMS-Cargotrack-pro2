0cdaf7da0b1a2811a6935d05e3f3f354c266b5d3
robertomsouza08-ctrl <robertomsouza08@gmail.com>
2025-10-29 15:51:44 -0300

    Update prisma.ts

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,8 +1,7 @@
 
-import './env'
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient()
 
