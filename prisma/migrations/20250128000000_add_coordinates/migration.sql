84f3439fdc6f158ef18b82fc7f67b766799aec7d
robertomsouza08-ctrl <robertomsouza08@gmail.com>
2025-10-29 13:11:12 -0300

    Delete prisma directory

--- prisma/migrations/20250128000000_add_coordinates/migration.sql
+++
@@ -1,6 +0,0 @@
-
--- AlterTable
-ALTER TABLE "Shipment" ADD COLUMN "originLat" REAL;
-ALTER TABLE "Shipment" ADD COLUMN "originLng" REAL;
-ALTER TABLE "Shipment" ADD COLUMN "destLat" REAL;
-ALTER TABLE "Shipment" ADD COLUMN "destLng" REAL;
